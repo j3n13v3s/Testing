5ae3d2b388063bb5f3dd0245c3838b60d1afd112
j3n13v3s <jenieves@ucompensar.edu.co>
2026-02-24 19:58:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const http = require('http');
-const port = process.env.PORT || 8080;
+const fs = require('fs');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
 const mensaje = process.env.WELCOME_MESSAGE || "¡Hola Mundo! Aplicación PaaS por defecto.";
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.end(`<h1>${mensaje}</h1><p>Esta app está corriendo en un servicio PaaS.</p>`);
+  // Leemos el archivo index.html
+  fs.readFile(path.join(__dirname, 'index.html'), 'utf8', (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.end('Error cargando el archivo index.html');
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+      // Reemplazamos la etiqueta por la variable de entorno
+      const htmlFinal = data.replace('{{WELCOME_MESSAGE}}', mensaje);
+      res.end(htmlFinal);
+    }
+  });
 });
 
 server.listen(port, () => {
